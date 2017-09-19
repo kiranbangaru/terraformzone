6ae4e3b6fa630c3e9cc57b2534f5d601304f3ce8
kiranbangaru <bangarukiran218@gmail.com>
2017-09-19 14:27:44 +0530

    added module call

---
+++ terraform/html-module-call/htmlmodule.tf
@@ -0,0 +1,6 @@
+module "html-module"{
+    source = "..\\modules\\html-module"
+    aws_access_key = "AKIAI65ONLYTMVFUU4AA"
+    aws_secret_key = "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"
+    private_key_path = "keypair.pem"
+}
